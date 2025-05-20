c52e650ccf8fb416962dd5b1b604602b57d302a8
Frosty-MacOs <al.korobko@yahoo.com>
2025-05-20 14:16:42 +0200

    @NotNull на поля у объектов БД

--- src/main/java/ru/practicum/shareit/item/Item.java
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -11,16 +11,19 @@ import lombok.Getter;
 @Data
 @Builder(toBuilder = true)
 public class Item {
+    @NotNull(message = "ошибка валидации, id не может быть null")
     @Positive(message = "ошибка валидации, id должно быть положительным числом")
     private Long id;
 
     // Здесь и далее: стоит ли ограничивать размер текстовых полей здесь, или же это можно опустить на БД?
     // Или же и там, и тут это делать, на всякий случай?
-    @NotBlank(message = "ошибка валидации, name не может быть null / Blank")
+    @NotNull(message = "ошибка валидации, name не может быть null")
+    @NotBlank(message = "ошибка валидации, name не может быть Blank")
     @Size(max = 100, message = "ошшибка валидации, длина name не может превышать 100 символов")
     private String name;
 
-    @NotBlank(message = "ошибка валидации, description не может быть null / Blank")
+    @NotNull(message = "ошибка валидации, description не может быть null")
+    @NotBlank(message = "ошибка валидации, description не может быть Blank")
     @Size(max = 1000, message = "ошшибка валидации, длина description не может превышать 1000 символов")
     private String description;
 
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -10,16 +11,17 @@ import lombok.Data;
 @Data
 @Builder(toBuilder = true)
 public class User {
-    //TODO у объектов БД @NotNull на поля?
-
+    @NotNull(message = "ошибка валидации, id не может быть null")
     @Positive(message = "ошибка валидации, id должно быть положительным числом")
     private Long id;
 
-    @NotBlank(message = "ошибка валидации, name не может быть null / Blank")
+    @NotNull(message = "ошибка валидации, name не может быть null")
+    @NotBlank(message = "ошибка валидации, name не может быть Blank")
     @Size(max = 100, message = "ошшибка валидации, длина name не может превышать 100 символов")
     private String name;
 
-    @NotBlank(message = "ошибка валидации, email не может быть null / Blank")
+    @NotNull(message = "ошибка валидации, email не может быть null")
+    @NotBlank(message = "ошибка валидации, email не может быть Blank")
     @Email(message = "ошибка валидации, email должен быть действительным")
     private String email;
 }
