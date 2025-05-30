ab9fcf386709774bed8285393c40120edc40c2d2
Frosty-MacOs <al.korobko@yahoo.com>
2025-05-30 15:20:41 +0200

    Скрипт для создания таблиц дополнен таблицей bookings. Новый enum класс State для Booking сущности

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS requests (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     description varchar(1000) NOT NULL,
-    -- не забывать, чтобы было на что ссылаться, эта таблица и поле уже должно существовать!!!
     request_id BIGINT NOT NULL,
     created_at date NOT NULL,
+    -- не забывать, чтобы было на что ссылаться, эта таблица и поле уже должно существовать!!!
     CONSTRAINT fk_requests_to_user FOREIGN KEY(request_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
@@ -23,4 +23,14 @@ CREATE TABLE IF NOT EXISTS items (
     availability boolean NOT NULL,
     CONSTRAINT fk_items_to_user FOREIGN KEY(owner_id) REFERENCES users(id) ON DELETE CASCADE,
     CONSTRAINT fk_items_to_request FOREIGN KEY(request_id) REFERENCES requests(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS bookings (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    item_id BIGINT NOT NULL,
+    booker_id BIGINT NOT NULL,
+    CONSTRAINT fk_bookings_to_item FOREIGN KEY(item_id) REFERENCES items(id) ON DELETE CASCADE,
+    CONSTRAINT fk_bookings_to_user FOREIGN KEY(booker_id) REFERENCES users(id) ON DELETE CASCADE
 );
