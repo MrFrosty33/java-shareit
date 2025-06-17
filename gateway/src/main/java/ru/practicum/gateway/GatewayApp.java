59e1240ab2cbcfc3aaddf694b6ea26c0a6e09549
Frosty-WinPC <al.korobko@yahoo.com>
2025-06-17 21:10:41 +0200

    Какие-то исправления. gateway контейнер не запускается :\\\

--- gateway/src/main/java/ru/practicum/gateway/GatewayApp.java
+++ gateway/src/main/java/ru/practicum/gateway/GatewayApp.java
@@ -3,7 +3,7 @@ package ru.practicum.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "ru.practicum.gateway")
+@SpringBootApplication(scanBasePackages = {"ru.practicum.gateway", "ru.practicum.models"})
 public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class);
