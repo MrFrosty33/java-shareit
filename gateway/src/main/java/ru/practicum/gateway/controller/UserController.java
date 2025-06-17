54ef15feced6733c5483ae8acf9b58ecbfe487c1
Frosty-WinPC <al.korobko@yahoo.com>
2025-06-17 16:38:19 +0200

    До изменений настроек проекта

--- gateway/src/main/java/ru/practicum/gateway/controller/UserController.java
+++ gateway/src/main/java/ru/practicum/gateway/controller/UserController.java
@@ -1,23 +1,18 @@
 package ru.practicum.gateway.controller;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import ru.practicum.models.markers.OnCreate;
 import ru.practicum.models.user.UserDto;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -25,45 +20,55 @@ import java.util.List;
 @Validated
 @Slf4j
 public class UserController {
+    private final RestTemplate restTemplate;
 
-    @GetMapping("/{id}")
-    public UserDto get(@PathVariable
-                       @NotNull(message = "ошибка валидации, id не может быть null")
-                       @Positive(message = "ошибка валидации, id должно быть положительным числом")
-                       Long id) {
-        return userService.get(id);
-    }
+    @Value("${SHAREIT_SERVER_URL}")
+    private String serverUrl;
 
-    @GetMapping
-    public List<UserDto> getAll() {
-        return userService.getAll();
-    }
+//    @GetMapping("/{id}")
+//    public UserDto get(@PathVariable
+//                       @NotNull(message = "ошибка валидации, id не может быть null")
+//                       @Positive(message = "ошибка валидации, id должно быть положительным числом")
+//                       Long id) {
+//        return userService.get(id);
+//    }
+//
+//    @GetMapping
+//    public List<UserDto> getAll() {
+//        return userService.getAll();
+//}
 
     @PostMapping
-    public UserDto save(@Validated(OnCreate.class) @RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> save(@Validated(OnCreate.class) @RequestBody UserDto userDto) {
         log.info("Начал выполнение метода save");
-        return userService.save(userDto);
+        String url = serverUrl + "/users";
+
+        ResponseEntity<UserDto> result = restTemplate.postForEntity(url, userDto, UserDto.class);
+
+        log.info("Выполнил метод save");
+        return ResponseEntity.status(result.getStatusCode()).body(result.getBody());
+
     }
 
-    @PatchMapping("/{id}")
-    public UserDto update(@Validated(OnUpdate.class) @RequestBody UserDto userDto,
-                          @PathVariable
-                          @NotNull(message = "ошибка валидации, id не может быть null")
-                          @Positive(message = "ошибка валидации, id должно быть положительным числом")
-                          Long id) {
-        return userService.update(userDto, id);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable
-                         @NotNull(message = "ошибка валидации, id не может быть null")
-                         @Positive(message = "ошибка валидации, id должно быть положительным числом")
-                         Long id) {
-        userService.delete(id);
-    }
-
-    @DeleteMapping
-    public void deleteAll() {
-        userService.deleteAll();
-    }
+//    @PatchMapping("/{id}")
+//    public UserDto update(@Validated(OnUpdate.class) @RequestBody UserDto userDto,
+//                          @PathVariable
+//                          @NotNull(message = "ошибка валидации, id не может быть null")
+//                          @Positive(message = "ошибка валидации, id должно быть положительным числом")
+//                          Long id) {
+//        return userService.update(userDto, id);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void delete(@PathVariable
+//                         @NotNull(message = "ошибка валидации, id не может быть null")
+//                         @Positive(message = "ошибка валидации, id должно быть положительным числом")
+//                         Long id) {
+//        userService.delete(id);
+//    }
+//
+//    @DeleteMapping
+//    public void deleteAll() {
+//        userService.deleteAll();
+//    }
 }
