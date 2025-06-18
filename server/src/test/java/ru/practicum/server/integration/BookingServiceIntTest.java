fa9d122793e0fe12a7e8269e954d0c9b786bd3ca
Frosty-WinPC <al.korobko@yahoo.com>
2025-06-18 17:38:53 +0200

    Написан интеграционный тест на BookingServiceImpl

--- server/src/test/java/ru/practicum/server/integration/BookingServiceIntTest.java
+++ server/src/test/java/ru/practicum/server/integration/BookingServiceIntTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.models.booking.BookingCreate;
 import ru.practicum.models.booking.BookingDto;
+import ru.practicum.models.booking.State;
 import ru.practicum.server.booking.BookingRepository;
 import ru.practicum.server.booking.BookingService;
 import ru.practicum.server.item.Item;
@@ -17,9 +18,11 @@ import ru.practicum.server.user.User;
 import ru.practicum.server.user.UserRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static ru.practicum.models.booking.Status.REJECTED;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -34,6 +37,11 @@ public class BookingServiceIntTest {
     private ItemRepository itemRepository;
     @Autowired
     private UserRepository userRepository;
+
+    private final LocalDateTime start =
+            LocalDateTime.of(2025, 6, 10, 14, 14, 0);
+    private final LocalDateTime end =
+            LocalDateTime.of(2025, 6, 17, 14, 14, 0);
 
     @BeforeEach
     void saveTestItemAndUser() {
@@ -55,9 +63,6 @@ public class BookingServiceIntTest {
 
     @Test
     void saveBooking_and_checkResult() {
-        LocalDateTime start = LocalDateTime.of(2025, 6, 10, 14, 14, 0);
-        LocalDateTime end = LocalDateTime.of(2025, 6, 17, 14, 14, 0);
-
         BookingCreate b = BookingCreate.builder()
                 .itemId(1L)
                 .start(start)
@@ -76,9 +81,6 @@ public class BookingServiceIntTest {
 
     @Test
     void saveBooking_and_getById() {
-        LocalDateTime start = LocalDateTime.of(2025, 6, 10, 14, 14, 0);
-        LocalDateTime end = LocalDateTime.of(2025, 6, 17, 14, 14, 0);
-
         BookingCreate b = BookingCreate.builder()
                 .itemId(1L)
                 .start(start)
@@ -94,5 +96,85 @@ public class BookingServiceIntTest {
         assertNotNull(result.getItem());
         assertEquals(start, result.getStart());
         assertEquals(end, result.getEnd());
+    }
+
+    @Test
+    void saveBookings_and_getAllByStateAndBookerId() {
+        BookingCreate b1 = BookingCreate.builder() // в прошлом
+                .itemId(1L)
+                .start(start)
+                .end(end)
+                .build();
+
+        BookingCreate b2 = BookingCreate.builder() // в будущем
+                .itemId(1L)
+                .start(start.plusDays(20))
+                .end(end.plusDays(25))
+                .build();
+
+        BookingCreate b3 = BookingCreate.builder() // в будущем
+                .itemId(1L)
+                .start(start.plusDays(10))
+                .end(end.plusDays(10))
+                .build();
+
+        service.save(b1, 1L);
+        service.save(b2, 1L);
+        service.save(b3, 1L);
+
+        List<BookingDto> result = service.getAllByStateAndBookerId(State.FUTURE, 1L);
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    void saveBookings_and_getAllByStateAndOwnerId() {
+        BookingCreate b1 = BookingCreate.builder() // в прошлом
+                .itemId(1L)
+                .start(start)
+                .end(end)
+                .build();
+
+        BookingCreate b2 = BookingCreate.builder() // в будущем
+                .itemId(1L)
+                .start(start.plusDays(20))
+                .end(end.plusDays(25))
+                .build();
+
+        BookingCreate b3 = BookingCreate.builder() // в будущем
+                .itemId(1L)
+                .start(start.plusDays(10))
+                .end(end.plusDays(10))
+                .build();
+
+        service.save(b1, 1L);
+        service.save(b2, 1L);
+        service.save(b3, 1L);
+
+        List<BookingDto> result = service.getAllByStateAndOwnerId(State.ALL, 1L);
+
+        assertNotNull(result);
+        assertEquals(3, result.size());
+    }
+
+    @Test
+    void saveBooking_and_approveBooking() {
+        BookingCreate b = BookingCreate.builder() // в прошлом
+                .itemId(1L)
+                .start(start)
+                .end(end)
+                .build();
+
+        Long id = service.save(b, 1L).getId();
+        BookingDto result = service.approveBooking(id, 1L, false);
+
+        assertNotNull(result);
+        assertEquals(1L, result.getId());
+        assertNotNull(result.getBooker());
+        assertNotNull(result.getItem());
+        assertEquals(start, result.getStart());
+        assertEquals(end, result.getEnd());
+        assertEquals(REJECTED, result.getStatus());
     }
 }
