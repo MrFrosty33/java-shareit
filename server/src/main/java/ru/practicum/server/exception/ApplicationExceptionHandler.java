a966b6ea4284e3edb649d186d73818bb9a847c5a
Frosty-WinPC <al.korobko@yahoo.com>
2025-06-19 23:12:25 +0200

    Убрал из ApplicationExceptionHandler обработку лишних Exception на стороне server

--- server/src/main/java/ru/practicum/server/exception/ApplicationExceptionHandler.java
+++ server/src/main/java/ru/practicum/server/exception/ApplicationExceptionHandler.java
@@ -1,13 +1,10 @@
 package ru.practicum.server.exception;
 
-import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.NoSuchElementException;
 
@@ -62,40 +59,6 @@ public class ApplicationExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleNoSuchElement(NoSuchElementException e) {
-        String exceptionName = e.getClass().getSimpleName();
-        String message = e.getMessage();
-
-        log.info("ApplicationExceptionHandler поймал {} с сообщением: {}",
-                exceptionName, message);
-        return new ErrorResponse(message);
-    }
-
-    // в последующих 3-х методах можно подумать над упрощением сообщения, чтобы оно не было несколько строк в длину
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
-        String exceptionName = e.getClass().getSimpleName();
-        String message = e.getMessage();
-
-        log.info("ApplicationExceptionHandler поймал {} с сообщением: {}",
-                exceptionName, message);
-        return new ErrorResponse(message);
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleConstraintViolation(ConstraintViolationException e) {
-        String exceptionName = e.getClass().getSimpleName();
-        String message = e.getMessage();
-
-        log.info("ApplicationExceptionHandler поймал {} с сообщением: {}",
-                exceptionName, message);
-        return new ErrorResponse(message);
-    }
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
         String exceptionName = e.getClass().getSimpleName();
         String message = e.getMessage();
 
