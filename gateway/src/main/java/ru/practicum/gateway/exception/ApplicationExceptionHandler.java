252e2ebb6c5c83204d70ea06d5a01970a8a09769
Frosty-WinPC <al.korobko@yahoo.com>
2025-06-19 23:04:12 +0200

    Убрал из ApplicationExceptionHandler обработку лишних Exception на стороне gateway и удалил лишние теперь классы

--- gateway/src/main/java/ru/practicum/gateway/exception/ApplicationExceptionHandler.java
+++ gateway/src/main/java/ru/practicum/gateway/exception/ApplicationExceptionHandler.java
@@ -2,7 +2,6 @@ package ru.practicum.gateway.exception;
 
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,21 +11,20 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Slf4j
 @RestControllerAdvice(basePackages = "ru.practicum.gateway")
 public class ApplicationExceptionHandler {
 
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleNotFound(NotFoundException e) {
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<ErrorResponse> handleHttpClientError(HttpClientErrorException e) {
+        // т.к. я не могу определить какой код ошибки изначательно содержится, не могу и выставить сразу @ResponseStatus
+        // потому этот метод будет несколько отличаться от остальных, но зато возвращать нужный ответ
         String exceptionName = e.getClass().getSimpleName();
         String message = e.getMessage();
 
-        log.info("ApplicationExceptionHandler поймал {} с сообщением: {}",
-                exceptionName, message);
-        return new ErrorResponse(message);
+        log.info("ApplicationExceptionHandler поймал {} с сообщением: {}", exceptionName, message);
+        return ResponseEntity.status(e.getStatusCode()).body(new ErrorResponse(message));
     }
 
     @ExceptionHandler
@@ -38,39 +36,6 @@ public class ApplicationExceptionHandler {
         log.info("ApplicationExceptionHandler поймал {} с сообщением: {}",
                 exceptionName, message);
         return new ErrorResponse(message);
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handleConflict(ConflictException e) {
-        String exceptionName = e.getClass().getSimpleName();
-        String message = e.getMessage();
-
-        log.info("ApplicationExceptionHandler поймал {} с сообщением: {}",
-                exceptionName, message);
-        return new ErrorResponse(message);
-    }
-
-    @ExceptionHandler({InternalServerException.class, Exception.class})
-    @ResponseStatus(INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleOther(Exception e) {
-        String exceptionName = e.getClass().getSimpleName();
-        String message = e.getMessage();
-
-        log.info("ApplicationExceptionHandler поймал {} с сообщением: {}",
-                exceptionName, message);
-        return new ErrorResponse(message);
-    }
-
-    @ExceptionHandler(HttpClientErrorException.class)
-    public ResponseEntity<ErrorResponse> handleHttpClientError(HttpClientErrorException e) {
-        // т.к. я не могу определить какой код ошибки изначательно содержится, не могу и выставить сразу @ResponseStatus
-        // потому этот метод будет несколько отличаться от остальных, но зато возвращать нужный ответ
-        String exceptionName = e.getClass().getSimpleName();
-        String message = e.getMessage();
-
-        log.info("ApplicationExceptionHandler поймал {} с сообщением: {}", exceptionName, message);
-        return ResponseEntity.status(e.getStatusCode()).body(new ErrorResponse(message));
     }
 
     // в последующих 3-х методах можно подумать над упрощением сообщения, чтобы оно не было несколько строк в длину
